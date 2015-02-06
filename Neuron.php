1a4a613aa6b7c89d13c2cf5ffa48044c83fcd1f1
Ruslan <aksyanov.r@gmail.com>
2015-02-06 18:33:21 +0400

    ready

--- Neuron.php
+++ Neuron.php
@@ -7,40 +7,90 @@ class Neuron
     public $inputMul = array(array());  //отмасштабированный input
     public $limit    = 9;               // лимит, ниже которой буква не верна
     public $inputSum = 0;               // сумма весов синапсисов
+    public $memoryFolder    = 'memory/';
+    public $memoryExt       = '.txt';
 
-    public function __construct(){
-        //в дальнешем загрузить память
-        $this->memory = array(array());
+    public function __construct($name){
+        $this->name = $name;
+        $this->loadInMemory();
     }
 
     public function mul(){
-
+        $y = 0;
+        foreach($this->memory as $elemY){
+            $x = 0;
+            foreach($elemY as $elemX){
+                $this->inputMul[$y][$x] = $this->input[$y][$x] * $elemX;
+                $x++;
+            }
+            $y++;
+        }
     }
 
     public function sum(){
-
+        $this->inputSum = 0;
+        foreach($this->inputMul as $elemY)
+            foreach($elemY as $elemX)
+                $this->inputSum+= $elemX;
     }
 
     public function isNeuron(){
-
+        return $this->inputSum >= $this->limit;
     }
 
-
-
-    public function OCR(){
-        $memory     = $this->memory;
-        $arrayOCR   = $this->arrayOCR;
-        for($y = 0;$y < count($memory);$y++){
-            for($x = 0;$x < count($memory[$y]);$x++){
-                if($memory[$y][$x] == $arrayOCR[$y][$x])
-                    $this->output++;
+    protected function loadInMemory(){
+        $filename = $this->memoryFolder.$this->name.$this->memoryExt;
+        if(file_exists($filename)){
+            $memoryFile = file($filename);
+            $y = 0;
+            $array = array(array());
+            foreach($memoryFile as $string){
+                $string = rtrim($string);
+                $arrayString = preg_split("/ /",$string,null,PREG_SPLIT_NO_EMPTY);
+                $array[$y] = $arrayString;
+                $y++;
             }
+            $this->memory = $array;
+        }else{
+            $stream = fopen($filename,'w');
+            $string = "0 0 0\r\n0 0 0\r\n0 0 0\r\n0 0 0\r\n0 0 0";
+            fwrite($stream,$string);
         }
-        echo 'output : '.$this->output.'<br>';
-        if($this->output > $this->minOutput)
-            echo $this->name;
     }
 
+    public function save(){
+        $filename = $this->memoryFolder.$this->name.$this->memoryExt;
+        $stream = fopen($filename,'w');
+        $string = "";
+        foreach($this->memory as $y){
+            foreach($y as $x){
+                $string.= $x.' ';
+            }
+            $string.="\r\n";
+        }
+        fwrite($stream,$string);
+    }
 
+    public function learn($input,$isNeuron){
+        $y = 0;
+        foreach($input as $elemY){
+            $x = 0;
+            foreach($elemY as $elemX){
+                if($isNeuron)
+                    $this->memory[$y][$x]+=$elemX;
+                else
+                    $this->memory[$y][$x]-=$elemX;
+                $x++;
+            }
+            $y++;
+        }
+    }
+
+    public function OCR($input){
+        $this->input = $input;
+        $this->mul();
+        $this->sum();
+        return $this->isNeuron();
+    }
 
 }
--- index.php
+++ index.php
@@ -2,153 +2,61 @@
 require_once('Neuron.php');
 require_once('ImageHelper.php');
 
-function OCR($arrayOfNeurons,$arrayOfImage){
+/*$n0 = new Neuron('0');
+$n1 = new Neuron('1');
+$n2 = new Neuron('2');
+$n3 = new Neuron('3');
+$n4 = new Neuron('4');*/
+$n5 = new Neuron('5');
 
-    foreach($arrayOfImage as $image){
-        foreach($arrayOfNeurons as $neuron){
-            $neuron->output = 0;
-        }
-        $arrayOCR = readImage($image);
-        for($y = 0;$y < count($arrayOCR);$y++){
-            for($x = 0;$x < count($arrayOCR[$y]);$x++){
-                foreach($arrayOfNeurons as $neuron){
-                    if($arrayOCR[$y][$x] > 0)
-                        //if($neuron->memory[$y][$x] == $arrayOCR[$y][$x])
-                        $neuron->output = $neuron->output + $neuron->memory[$y][$x];
-                }
+$dir = 'images/3x5/';
+
+for($iter = 0;$iter < 0;$iter++){
+    echo 'Итерация обучения №'.$iter.'<br>';
+    if(is_dir($dir))
+        if($dh = opendir($dir))
+            while(($file = readdir($dh))!== false){
+                if(filetype($dir.$file) == 'dir')
+                    continue;
+
+                $image = new ImageHelper($dir.$file);
+                $imageArray = $image->array;
+                $isNeuron = $n5->OCR($imageArray);
+
+               /* if(strripos($file,'5') > -1){ //цифра 5
+                    if(!$isNeuron)
+                        $n5->learn($imageArray,true);
+                }else{
+                    if($isNeuron)
+                        $n5->learn($imageArray,false);
+                }*/
             }
-        }
 
-        $answer = '!_err';
-        $maxOutput = 0;
-        foreach($arrayOfNeurons as $neuron){
-            if($neuron->output > $maxOutput){
-                $maxOutput = $neuron->output;
-                $answer = $neuron->name;
-            }
-        }
-
-        echo $answer;
-    }
-
-    echo '<br>';
-
+    $n5->save();
 }
 
-function resize($img,$width,$height){
+echo 'Чтение и распознование цифры 5<br>';
+if(is_dir($dir))
+    if($dh = opendir($dir))
+        while(($file = readdir($dh))!== false){
+            if(filetype($dir.$file) == 'dir')
+                continue;
 
-    // Проверяем корректность введения ширины и высоты
-    $width=filter_var($width,FILTER_VALIDATE_INT)?$width:false;
-    $height=filter_var($height,FILTER_VALIDATE_INT)?$height:false;
+            $image = new ImageHelper($dir.$file);
+            $imageArray = $image->array;
+            $isNeuron = $n5->OCR($imageArray);
 
-    // Если ширина не указана или указана некорректно, то будет игнорироватся изменение масштаба изображения
-    if(($width!='100'&&$height==false) or ($width!=false&&$height!=false)){
-        $w_src = ImageSX($img);
-        $h_src = ImageSY($img);
-
-        // Если не указана высота, то по процентному соотношению вычисляем новый масштаб
-        if($height==false){
-            $height=($h_src/100)*$width;
-            $width=($w_src/100)*$width;
+            echo $file.': ';
+            var_dump($isNeuron);
+            echo '<br>';
         }
 
-        $dest = imagecreatetruecolor($width,$height);
-
-        if($height==$width){
-            if ($w_src>$h_src) {
-                imagecopyresized($dest, $img, 0, 0, round((max($w_src,$h_src)-min($w_src,$h_src))/2), 0, $width, $height, min($w_src,$h_src), min($w_src,$h_src));
-            } else {
-                imagecopyresized($dest, $img, 0, 0, 0, round((max($w_src,$h_src)-min($w_src,$h_src))/2), $width, $height, min($w_src,$h_src), min($w_src,$h_src));
-            }
-        } else {
-            imagecopyresized($dest, $img, 0, 0, 0, 0, $width, $height, $w_src, $h_src);
-        }
-
-        $img=$dest;
-        unset($dest);
-
-    }
-}
-
-function readImage($pathToImg){
-    $image = imagecreatefromjpeg($pathToImg);
-
-    resize($image,50,50);
-
-    $maxX = imagesx($image);
-    $maxY = imagesy($image);
-
-    $array = array(array());
-
-    for($y = 0;$y < $maxY;$y++){
-        for($x = 0;$x < $maxX;$x++){
-            $array[$y][$x] = blackOrWhite(imagecolorsforindex($image,imagecolorat ($image , $x, $y)));
-        }
-    }
-    imagedestroy($image);
-
-    return $array;
-}
-
-function blackOrWhite($arrayColor){
-    if($arrayColor['red'] >= 150 && $arrayColor['green'] >= 150 && $arrayColor['blue'] >= 150)
-        return '1';
-    else
-        return '0';
-}
-
-/*$neuronA = new Neuron();
-$neuronA->name = 'А';
-$neuronA->readImage('testA1.jpg',true);
-$neuronA->readImage('2.jpg',true);
-
-$neuronB = new Neuron();
-$neuronB->name = 'Б';
-$neuronB->readImage('testB1.jpg',true);
-
-$neuronV = new Neuron();
-$neuronV->name = 'В';
-$neuronV->readImage('testV1.jpg',true);
-$neuronV->printImage(true);
-
-$neuronA1 = new Neuron();
-$neuronA1->name = 'A1';
-$neuronA1->readImage('2.jpg',true);
-$neuronA1->printImage(true);
-
-$neuronA->printImage(true);
-
-$arrayOfNeurons = array($neuronA,$neuronB,$neuronV);
-
-//1 test
-$arrayOfImage = array('testA1.jpg','testA2.jpg','testV2.jpg','testV1.jpg');
-OCR($arrayOfNeurons,$arrayOfImage);
-
-//2 test
-$arrayOfImage = array('testB1.jpg','testV2.jpg','testV1.jpg','testA1.jpg');
-OCR($arrayOfNeurons,$arrayOfImage);
-
-//3 test
-$arrayOfImage = array('testB1.jpg','testV2.jpg','testV1.jpg','testB1.jpg');
-OCR($arrayOfNeurons,$arrayOfImage);
-
-//4 test
-$arrayOfImage = array('testA1Big.jpg');
-OCR($arrayOfNeurons,$arrayOfImage);*/
+echo '<br><br>В памяти:<br><pre>';
+print_r($n5->memory);
+echo '</pre>';
 
 
-
-/*$image = imagecreatefromjpeg('testA1Big.jpg');
-$width = 50;
-$height = 50;
-$w_src = ImageSX($image);
-$h_src = ImageSY($image);
-$dest = imagecreatetruecolor($width,$height);
-echo imagecopyresized($dest, $image, 0, 0, 0, 0, $width, $height, $w_src, $w_src);*/
-
-
-$image = new ImageHelper('images/3x7/2.png');
-$image->printArray();
+?>
 
 
 
